738db589fe46559c56a7dc2ecf955d3cca7a1ec0
Adam Wojtysiak <41833220+yamataro22@users.noreply.github.com>
2019-03-02 16:39:30 +0100

    Created SettingsTestActivity. Not implemented yet.

---
+++ app/src/main/java/com/example/stereovisioncarsystem/CommunicationTestActivity.java
@@ -0,0 +1,13 @@
+package com.example.stereovisioncarsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommunicationTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_communication_test);
+    }
+}
